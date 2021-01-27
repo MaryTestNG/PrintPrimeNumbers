c128818ee7f0c869018c534af208cd02db55f616
Kiran <kiranreddyamk@gmail.com>
2021-01-26 23:18:16 -0500

    Update PrintPrimeNumbersApplicationTests.java

--- src/test/java/com/ap/nellore/PrintPrimeNumbersApplicationTests.java
+++ src/test/java/com/ap/nellore/PrintPrimeNumbersApplicationTests.java
@@ -12,7 +12,7 @@ class PrintPrimeNumbersApplicationTests {
 	
 	@Test
 	void test1() {
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 20; i++) {
 			System.out.println("Hello "+ i);
 		}
 	}
